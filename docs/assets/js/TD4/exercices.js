6bb74d485e8a32befc8f9295c96e311ed5d5aa45
arthco14 <arthco31@gmail.com>
2024-09-30 11:25:54 +0200

    Update exercices.js

--- docs/assets/js/TD4/exercices.js
+++ docs/assets/js/TD4/exercices.js
@@ -1,5 +1,5 @@
 // Fonction pour changer la couleur de fond de la page
-document.getElementById('changeColorBtn').addEventListener('click', function() {
-    document.body.style.backgroundColor = 
-        '#' + Math.floor(Math.random() * 16777215).toString(16);
-});
+function changeColor(color) { 
+    document.body.style.background = color; 
+};
+
